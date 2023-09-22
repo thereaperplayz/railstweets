91d7ec1c8eda6fae206a4c45e44dbe3e0a2df02a
Micah Criddle <micahcriddle@gmail.com>
2023-09-22 16:02:52 -0600

    finishing the sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 root to: "main#index"
 get '/about', to: "about#index"
+
+get '/sign_up', to: "registrations#new"
+post "sign_up", to: "registrations#create"
   # Defines the root path route ("/")
   # root "articles#index"
 end
