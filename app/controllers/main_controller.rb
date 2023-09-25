137365e0de22b0f51d3369a58a6c22e691eec7ff
Micah Criddle <micahcriddle@gmail.com>
2023-09-25 17:23:50 -0600

    adding functionality for login errors

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,8 @@
 class MainController < ApplicationController
     def index
+      if session[:user_id]
+        @user = User.find_by(id: session[:user_id])
+      end
     end
-end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ get '/about', to: "about#index"
 
 get '/sign_up', to: "registrations#new"
 post "sign_up", to: "registrations#create"
+
+get '/sign_in', to: "sessions#new"
+post '/sign_in', to: "sessions#create"
+delete "logout", to: "sessions#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
 end
